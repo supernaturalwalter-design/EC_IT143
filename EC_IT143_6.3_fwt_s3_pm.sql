d1ad49f3b022238d9cae65edaf991431a86ad800
Pedzisai Walter Machisvo <supernaturalwalter@gmail.com>
2026-06-15 22:05:58 +0200

    Rename EC_IT143_6.3_fwt_s3_pm.sql to FunWithFunctions/EC_IT143_6.3_fwt_s3_pm.sql

--- EC_IT143_6.3_fwt_s3_pm.sql
+++
@@ -1,6 +0,0 @@
--- Step 3: Research
-
--- Triggers run automatically after updates.
-
--- Resource:
--- https://learn.microsoft.com/en-us/sql/t-sql/statements/create-trigger-transact-sql
---
+++ FunWithFunctions/EC_IT143_6.3_fwt_s3_pm.sql
@@ -0,0 +1,6 @@
+-- Step 3: Research
+
+-- Triggers run automatically after updates.
+
+-- Resource:
+-- https://learn.microsoft.com/en-us/sql/t-sql/statements/create-trigger-transact-sql
